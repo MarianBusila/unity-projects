16acfb83cdc6f0640cade4a3d897eda4b8508173
Marian Busila <marian@imposition.com>
2016-07-09 19:08:44 -0400

    MultiplayerBasic

---
+++ MultiplayerBasic/Assets/Scripts/Bullet.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bullet : MonoBehaviour {
+
+	void OnCollisionEnter(Collision collision)
+    {
+        GameObject hit = collision.gameObject;
+        Health health = hit.GetComponent<Health>();
+        if(health != null)
+            health.TakeDamage(20);
+        Destroy(gameObject);
+    }
+}
---
+++ MultiplayerBasic/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+
+public class PlayerController : NetworkBehaviour {
+
+    public GameObject bulletPrefab;
+    public Transform bulletSpawn;
+    void Update()
+    {
+        if (!isLocalPlayer)
+            return;
+
+        float x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
+        float z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
+
+        transform.Rotate(0, x, 0);
+        transform.Translate(0, 0, z);
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            CmdFire();
+        }
+    }
+
+    [Command]
+    void CmdFire()
+    {
+        GameObject bullet = (GameObject)Instantiate(bulletPrefab, bulletSpawn.position, bulletSpawn.rotation);
+        //add velocity to bullet
+        bullet.GetComponent<Rigidbody>().velocity = bullet.transform.forward * 6.0f;
+        //Spawn the bullet on the Clients
+        NetworkServer.Spawn(bullet);
+
+        //destroy bullet after 2 seconds
+        Destroy(bullet, 2);
+    }
+
+    public override void OnStartLocalPlayer()
+    {
+        GetComponent<MeshRenderer>().material.color = Color.blue;
+    }
+}
