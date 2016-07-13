86ffc3c5ffa5ec5bad25d88981ded7639e264ead
Marian Busila <marian.busila@gmail.com>
2016-07-12 23:08:39 -0400

    HorrorApartment - FlashLight

---
+++ HorrorApartment/Assets/Scripts/Flashlight.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Flashlight : MonoBehaviour {
+
+    Light flashLight;
+    private bool isActive;
+    AudioSource audioSource;
+
+    public AudioClip soundFlashlightOn;
+    public AudioClip soundFlashlightOff;
+
+    // Use this for initialization
+    void Start () {
+        isActive = true;
+        audioSource = GetComponent<AudioSource>();
+        flashLight = GetComponent<Light>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if(Input.GetKeyDown(KeyCode.F))
+        {
+            isActive = !isActive;
+            flashLight.enabled = isActive;
+            if (isActive)
+                audioSource.PlayOneShot(soundFlashlightOn);
+            else
+                audioSource.PlayOneShot(soundFlashlightOff);
+
+        }
+	}
+}
