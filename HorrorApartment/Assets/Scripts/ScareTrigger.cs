03e62053e96dd146f109e4cd1cad47752be92735
Marian Busila <marian.busila@gmail.com>
2016-07-13 01:29:39 -0400

    HorrorApartment - ScareTrigger

---
+++ HorrorApartment/Assets/Scripts/ScareTrigger.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScareTrigger : MonoBehaviour {
+
+    public AudioSource scareAudioSource;
+    bool hasPlayedAudio;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player") && !hasPlayedAudio)
+        {
+            scareAudioSource.Play();
+            hasPlayedAudio = true;
+        }
+    }
+}
