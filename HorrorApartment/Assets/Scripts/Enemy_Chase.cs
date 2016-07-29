bd8851830f13858a77e9d76bd53e1cccec13d506
Marian Busila <marian.busila@gmail.com>
2016-07-29 17:28:30 -0400

    HorrorApartment - NavMesh

---
+++ HorrorApartment/Assets/Scripts/Enemy_Chase.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy_Chase : MonoBehaviour {
+
+    private NavMeshAgent myAgent;
+    public Transform transformTarget;
+
+	// Use this for initialization
+	void Start () {
+        myAgent = GetComponent<NavMeshAgent>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        myAgent.SetDestination(transformTarget.position);
+	
+	}
+}
