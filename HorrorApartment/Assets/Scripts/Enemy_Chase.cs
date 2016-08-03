8c971c3e65b7fbe631db16b0e5809db4ded0fc60
Marian Busila <marian.busila@gmail.com>
2016-08-02 21:20:29 -0400

    HorrorApartment - Animations

--- HorrorApartment/Assets/Scripts/Enemy_Chase.cs
+++ HorrorApartment/Assets/Scripts/Enemy_Chase.cs
@@ -4,16 +4,61 @@ using System.Collections;
 public class Enemy_Chase : MonoBehaviour {
 
     private NavMeshAgent myAgent;
+    private Animator myAnimator;
     public Transform transformTarget;
+
+    public bool chaseTarget = true;
+    public float stoppingDistance = 2.5f;
+    public float delayBetweenAttacks = 1.5f;
+    private float attackCooldown;
+
+    private float distanceFromTarget;
 
 	// Use this for initialization
 	void Start () {
         myAgent = GetComponent<NavMeshAgent>();
+        myAnimator = GetComponent<Animator>();
+        myAgent.stoppingDistance = stoppingDistance;
+        attackCooldown = Time.time;
 	}
 	
 	// Update is called once per frame
-	void Update () {
-        myAgent.SetDestination(transformTarget.position);
-	
+	void Update ()
+    {
+        ChaseTarget();
 	}
+
+    void ChaseTarget()
+    {
+        distanceFromTarget = Vector3.Distance(transformTarget.position, transform.position);
+        if(distanceFromTarget >= stoppingDistance)
+        {
+            chaseTarget = true;
+        }
+        else
+        {
+            chaseTarget = false;
+            Attack();
+        }
+
+        if (chaseTarget)
+        {
+            myAgent.SetDestination(transformTarget.position);
+            myAnimator.SetBool("isChasing", true);
+        }
+        else
+        {
+            myAnimator.SetBool("isChasing", false);
+        }
+    }
+
+    void Attack()
+    {
+        if(Time.time > attackCooldown)
+        {
+            Debug.Log("Attack");
+            myAnimator.SetTrigger("Attack");
+            attackCooldown = Time.time + delayBetweenAttacks;
+        }
+    }
 }
