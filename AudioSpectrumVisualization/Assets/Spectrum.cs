2958b9d44560ee2cd3a1476f387c9629e3bcb396
Marian Busila <marian.busila@gmail.com>
2016-09-25 00:04:33 -0400

    Audio Spectrum Visualization

---
+++ AudioSpectrumVisualization/Assets/Spectrum.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spectrum : MonoBehaviour {
+
+    public GameObject prefab;
+    public int numberOfObjects = 20;
+    public float radius = 5f;
+
+    GameObject[] cubes;
+    float speed = 10f;
+
+    void Start()
+    {
+        cubes = new GameObject[numberOfObjects];
+        for (int i = 0; i < numberOfObjects; i++)
+        {
+            float angle = i * Mathf.PI * 2 / numberOfObjects;
+            Vector3 pos = new Vector3(Mathf.Cos(angle), 0, Mathf.Sin(angle)) * radius;
+            GameObject cube = Instantiate(prefab, pos, Quaternion.identity) as GameObject;
+            cubes[i] = cube;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float[] samples = new float[1024];
+        AudioListener.GetSpectrumData(samples, 0, FFTWindow.Hamming);
+        for (int i = 0; i < numberOfObjects; i++)
+        {
+            Vector3 previousScale = cubes[i].transform.localScale;
+            previousScale.y = Mathf.Lerp(previousScale.y, samples[i] * 40, Time.deltaTime * speed);
+            cubes[i].transform.localScale = previousScale;
+        }
+    }
+}
