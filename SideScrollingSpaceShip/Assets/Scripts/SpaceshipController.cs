6c71b142a7c45b9b614bad8a18cdcb13cfbae49f
Marian Busila <marian.busila@gmail.com>
2016-11-30 23:13:09 -0500

    SideScrollingSpaceShip - Spaceship controller

---
+++ SideScrollingSpaceShip/Assets/Scripts/SpaceshipController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpaceshipController : MonoBehaviour {
+
+    public float speed = 2.0f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+		
+	void FixedUpdate () {
+        Vector3 moveDirection = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
+        transform.position += moveDirection * Time.deltaTime * speed;
+	}
+}
