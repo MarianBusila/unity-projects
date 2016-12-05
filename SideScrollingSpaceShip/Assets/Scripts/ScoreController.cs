2f974ce3394f077883cd5fc66eb20ff27383ca88
Marian Busila <marian.busila@gmail.com>
2016-12-04 23:59:35 -0500

    SideScrollingSpaceShip - Score

---
+++ SideScrollingSpaceShip/Assets/Scripts/ScoreController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ScoreController : MonoBehaviour {
+
+    public Text scoreText;
+    private float speed;
+    private SectionMovement sm;
+    // Use this for initialization
+    void Start () {
+         sm = GetComponent<SectionMovement>();        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        speed = sm.speed;
+        scoreText.text = "Score: " + Mathf.Round(Time.time * 10 * speed);
+	}
+}
