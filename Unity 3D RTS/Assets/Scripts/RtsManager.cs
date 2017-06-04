7e481ffb4bfc45f6b62bf40471e189b9bdc1ab7c
Marian Busila <marian.busila@gmail.com>
2017-06-04 01:37:00 -0400

    Unity 3D RTS - Initial Setup

---
+++ Unity 3D RTS/Assets/Scripts/RtsManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RtsManager : MonoBehaviour {
+
+    public static RtsManager Current = null;
+
+    public List<PlayerSetupDefinition> Players = new List<PlayerSetupDefinition>();
+
+	// Use this for initialization
+	void Start () {
+        Current = this;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
