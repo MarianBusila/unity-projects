9b8d683b5ac16da3b3d0c1f090fbf8f34c9b5d9e
Marian Busila <marian.busila@gmail.com>
2017-06-05 22:46:36 -0400

    Unity 3D RTS - Add Unit and Unit selection

---
+++ Unity 3D RTS/Assets/Scripts/MouseManager.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseManager : MonoBehaviour {
+
+    private List<Interactive> Selections = new List<Interactive>();	
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        // mouse button not pressed
+        if(!Input.GetMouseButtonDown(0))
+        {
+            return;
+        }
+
+        // skip if clicked on a 2d UI element like a button
+        var es = UnityEngine.EventSystems.EventSystem.current;
+        if(es != null && es.IsPointerOverGameObject())
+        {
+            return;
+        }
+
+        if(Selections.Count > 0)
+        {
+            // shift is not pressed
+            if( !Input.GetKey(KeyCode.LeftShift) && !Input.GetKey(KeyCode.RightShift))
+            {
+                foreach (var sel in Selections)
+                {
+                    if (sel != null)
+                    {
+                        sel.Deselect();
+                    }
+                }
+                Selections.Clear();
+            }
+        }
+
+        var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if(!Physics.Raycast(ray, out hit))
+        {
+            return;
+        }
+
+        var interact = hit.transform.GetComponent<Interactive>();
+        if(interact == null)
+        {
+            return;
+        }
+
+        Selections.Add(interact);
+        interact.Select();		
+	}
+}
