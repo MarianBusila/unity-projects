bea2b428b165d6440314b96b0aa9b22fff59fe0b
Marian Busila <marian.busila@gmail.com>
2017-06-18 17:16:07 -0400

    Unity 3D RTS - UnitInfo

---
+++ Unity 3D RTS/Assets/Scripts/Interactions/ShowUnitInfo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowUnitInfo : Interaction {
+
+    public string Name;
+    public float MaxHealth, CurrentHealth;
+    public Sprite ProfilePic;
+
+    public override void Deselect()
+    {
+        InfoManager.Current.ClearPic();
+        InfoManager.Current.ClearLines();
+    }
+
+    public override void Select()
+    {
+        InfoManager.Current.SetPic(ProfilePic);
+        InfoManager.Current.SetLines(Name, CurrentHealth.ToString() + " / " + MaxHealth.ToString(), "Owner: " + GetComponent<Player>().Info.Name);
+    }
+}
