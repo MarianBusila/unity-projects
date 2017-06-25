992a6c40aa13d92521d4290b3416dd6fb6b344dc
Marian Busila <marian.busila@gmail.com>
2017-06-25 11:58:22 -0400

    Unity 3D RTS - Command Base Prefab and Building follows mouse

--- Unity 3D RTS/Assets/Scripts/Actions/CreateBuildingAction.cs
+++ Unity 3D RTS/Assets/Scripts/Actions/CreateBuildingAction.cs
@@ -5,11 +5,39 @@ using UnityEngine;
 
 public class CreateBuildingAction : ActionBehaviour
 {
+    public GameObject GhostBuildingPrefab;
+    private GameObject active = null;
+
     public override Action GetClickAction()
     {
         return delegate()
         {
-            Debug.Log("Create Command Base Attempt");
+            var go = GameObject.Instantiate(GhostBuildingPrefab);
+            go.AddComponent<FindBuildingSite>();
+            active = go;
         };
+    }
+
+    private void Update()
+    {
+        if (active == null)
+        {
+            return;
+        }
+
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            GameObject.Destroy(active);
+        }
+    }
+
+    private void OnDestroy()
+    {
+        if(active == null)
+        {
+            return;
+        }
+
+        Destroy(active);
     }
 }
