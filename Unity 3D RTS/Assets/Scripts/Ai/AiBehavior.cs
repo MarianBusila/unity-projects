fac26110962296ef83ffda19b355d4ff8a90edbe
Marian Busila <marian.busila@gmail.com>
2017-07-17 23:37:30 -0400

    Unity 3D RTS - AI Controller

---
+++ Unity 3D RTS/Assets/Scripts/Ai/AiBehavior.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class AiBehavior : MonoBehaviour {
+
+    public abstract float GetWeight();
+
+    public abstract float Execute(); 
+}
---
+++ Unity 3D RTS/Assets/Scripts/Ai/AiController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AiController : MonoBehaviour {
+
+    private List<AiBehavior> Ais = new List<AiBehavior>();
+	// Use this for initialization
+	void Start () {
+		foreach(var ai in GetComponents<AiBehavior>())
+            Ais.Add(ai);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float bestAiValue = float.MinValue;
+        AiBehavior bestAi = null;
+
+        foreach(var ai in Ais)
+        {
+            var aiValue = ai.GetWeight();
+            if(aiValue > bestAiValue)
+            {
+                bestAi = ai;
+                bestAiValue = aiValue;
+            }
+        }
+
+        bestAi.Execute();
+	}
+}
