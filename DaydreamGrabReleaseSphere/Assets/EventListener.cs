353aa9c9e6411062f553fc4edf7508ae556d02f5
Marian Busila <marian.busila@gmail.com>
2018-04-01 19:11:45 -0400

    DaydreamGrabReleaseSphere - Change sphere color on enter and on exit with controller cursor

---
+++ DaydreamGrabReleaseSphere/Assets/EventListener.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventListener : MonoBehaviour {
+
+    private Renderer _renderer;
+	// Use this for initialization
+	void Start () {
+        _renderer = gameObject.GetComponent<Renderer>();
+	}
+
+    public void OnEnter()
+    {
+        _renderer.material.color = Color.red;
+    }
+
+    public void OnExit()
+    {
+        _renderer.material.color = Color.white;
+    }
+}
