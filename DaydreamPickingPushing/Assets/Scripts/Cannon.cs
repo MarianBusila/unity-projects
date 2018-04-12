5c58000c2ab96a713bb1f6ea752fa07d54ed579a
Marian Busila <marian.busila@gmail.com>
2018-04-11 22:02:35 -0400

    DaydreamPickingPushing - Fire cannonball when button is pressed

--- DaydreamPickingPushing/Assets/Scripts/Cannon.cs
+++ DaydreamPickingPushing/Assets/Scripts/Cannon.cs
@@ -3,14 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Cannon : MonoBehaviour {
+    private float _force = 20;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    protected Vector3 Velocity
+    {
+        get { return transform.localToWorldMatrix * Vector3.up * _force; }
+    }
+
+    public GameObject cannonballPrefab;
 	
-	// Update is called once per frame
-	void Update () {
-		
+	public void Fire () {
+        // create a cannonball at the current position and rotation
+        GameObject ball = Instantiate(cannonballPrefab, transform.position, transform.rotation);
+
+        // get the rigidbody physics component
+        var cannonballRigidbody = ball.GetComponent<Rigidbody>();
+
+        // apply the velocity
+        cannonballRigidbody.AddForce(Velocity, ForceMode.VelocityChange);
 	}
 }
