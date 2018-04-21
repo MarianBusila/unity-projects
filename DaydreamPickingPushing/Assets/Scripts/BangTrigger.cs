3006e6d2bab07cf132b1c5155567170f42086491
Marian Busila <marian.busila@gmail.com>
2018-04-20 23:33:51 -0400

    DaydreamPickingPushing - Added sound and icon for app

---
+++ DaydreamPickingPushing/Assets/Scripts/BangTrigger.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BangTrigger : MonoBehaviour {
+
+    private AudioSource bangAudioSource;
+
+    private void Start()
+    {
+        bangAudioSource = GetComponent<AudioSource>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        bangAudioSource.Play();
+        Debug.Log("Bang");
+    }
+}
