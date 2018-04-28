3519fbb653351c4fcbf964b5dcf733fa75ed02cf
Marian Busila <marian.busila@gmail.com>
2018-04-28 08:30:32 -0400

    ARCorePortal - Portal and PortalCamera prefabs in the park scene

--- ARCorePortal/Assets/Scripts/InterdimensionalTransport.cs
+++ ARCorePortal/Assets/Scripts/InterdimensionalTransport.cs
@@ -18,7 +18,7 @@ public class InterdimensionalTransport : MonoBehaviour
 
     private void OnTriggerStay(Collider other)
     {
-        if(other.name != "Main Camera")
+        if(other.name != "PortalCamera")
         {
             return;
         }
