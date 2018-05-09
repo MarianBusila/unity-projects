ef28243a7af56d825b5133f4ef7de551b6281def
Marian Busila <marian.busila@gmail.com>
2018-05-09 01:10:43 -0400

    MLAgentsPushBlock - PushBlockAgent draft

---
+++ MLAgents/Assets/Examples/PushBlock/Scripts/PushBlockAgent.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PushBlockAgent : Agent {
+
+    public GameObject ground;
+
+    public GameObject area;
+
+    [HideInInspector]
+    public Bounds areaBounds;
+
+    PushBlockAcademy academy;
+
+    // the goal to push the block to
+    public GameObject goal;
+
+    // the block to be pushed to the goal
+    public GameObject block;
+    
+    GoalDetect goalDetect;
+
+    Rigidbody blockRB;
+    Rigidbody agentRB;
+    Material groundMaterial;
+    RayPerception rayPerception;
+
+    // will change the material based on success/failure
+    Renderer groundRenderer;
+
+    private void Awake()
+    {
+        academy = FindObjectOfType<PushBlockAcademy>();
+    }
+
+    public override void InitializeAgent()
+    {
+        
+    }
+
+    public override void CollectObservations()
+    {
+        
+    }
+
+    public override void AgentAction(float[] vectorAction, string textAction)
+    {
+        
+    }
+
+    public override void AgentReset()
+    {
+        
+    }
+}
