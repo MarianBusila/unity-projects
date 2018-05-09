9233f34d92864279e3dba54743354c7d8dc05c4c
Marian Busila <marian.busila@gmail.com>
2018-05-09 00:31:54 -0400

    MLAgentsPushBlock - Academy

---
+++ MLAgents/Assets/Examples/PushBlock/Scripts/PushBlockAcademy.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PushBlockAcademy : Academy {
+
+    // the walking speed of agent in the scene
+    public float agentRunSpeed;
+
+    public float agentRotationSpeed;
+
+    // .9 means 90% of spawn area will be used
+    public float spawnAreaMarginMultiplier;
+
+    // when a goal is scored the ground will change
+    public Material goalScoredMaterial;
+
+    // when agent fails, the ground will change
+    public Material failMaterial;
+
+    public float gravityMultiplier = 1.0f;
+
+    void State()
+    {
+        Physics.gravity *= gravityMultiplier;
+    }
+}
