8abca84d36cbe226d92c2bc6138a0ce1e4505496
Marian Busila <marian.busila@gmail.com>
2018-05-26 09:05:32 -0400

    MLAgentsTennis - TennisAcademy and TennisArea script for resetting the match

---
+++ MLAgents/Assets/Examples/Tennis/Scripts/TennisArea.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TennisArea : MonoBehaviour {
+
+    public GameObject ball;
+    public GameObject agentA;
+    public GameObject agentB;
+
+    void Start()
+    {
+        MatchReset();
+    }
+
+    public void MatchReset()
+    {
+        float ballResetXPosition = Random.Range(6f, 8f);
+        float ballResetYPosition = 6f;
+        int flip = Random.Range(0, 2);
+
+        if(flip == 0)
+        {
+            // ball should be in the agentA's zone
+            ball.transform.position = new Vector3(-ballResetXPosition, ballResetYPosition, 0f) + transform.position;
+        }
+        else
+        {
+            // ball should be in the agentB's zone
+            ball.transform.position = new Vector3(ballResetXPosition, ballResetYPosition, 0f) + transform.position;
+        }
+
+        ball.GetComponent<Rigidbody>().velocity = new Vector3(0f, 0f, 0f);
+        // ball.GetComponent<HitWall>().lastAgentHit = -1;
+    }
+
+    private void FixedUpdate()
+    {
+        // clamp velocity
+        float clampVelocity = 9f;
+        Vector3 rbVelocity = ball.GetComponent<Rigidbody>().velocity;
+        ball.GetComponent<Rigidbody>().velocity = new Vector3(Mathf.Clamp(rbVelocity.x, -clampVelocity, clampVelocity), Mathf.Clamp(rbVelocity.y, -clampVelocity, clampVelocity), rbVelocity.z);
+    }
+}
