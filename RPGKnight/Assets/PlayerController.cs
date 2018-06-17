1fda57533811124dd1fd252255a0c2a535aa32de
Marian Busila <marian.busila@gmail.com>
2018-06-17 07:43:22 -0400

    RPGKnight - player controller script to raycast a ray to the left mouse position where we left clicked. Add a Ground layer for the movement mask.

---
+++ RPGKnight/Assets/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    Camera cam;
+    public LayerMask movementMask;
+
+	// Use this for initialization
+	void Start () {
+        cam = Camera.main;	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        // left mouse button
+		if(Input.GetMouseButtonDown(0))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            float maxDistance = 100;
+            if(Physics.Raycast(ray, out hit, maxDistance, movementMask))
+            {
+                // move player to what we hit
+                Debug.Log("We hit " + hit.collider.name + " " + hit.point);
+
+                // stop focusing any objects
+            }
+        }
+	}
+}
