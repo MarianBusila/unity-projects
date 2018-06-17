287bec23f08e8232bf66c74c9d07bc623174e0b8
Marian Busila <marian.busila@gmail.com>
2018-06-17 08:31:09 -0400

    RPGKnight - camera controller script to follow player

---
+++ RPGKnight/Assets/CameraController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+    public Transform target;
+    public Vector3 offsetTarget;
+    public float pitch = 2f;
+    private float currentZoom = 10;
+
+	
+	void LateUpdate () {
+        // camera will follow the target
+        transform.position = target.position - offsetTarget * currentZoom;
+
+        // look at target
+        transform.LookAt(target.position + Vector3.up * pitch);
+	}
+}
