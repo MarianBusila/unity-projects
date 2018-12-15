f00d11f8faf0bdb55c8b054ea81c89f927447022
Marian Busila <marian.busila@gmail.com>
2018-12-15 08:57:44 -0500

    MLAgentsReacherArm - create Agent object with shoulder, upper and lower arm and configurable joints

---
+++ MLAgentsReacherArm/Assets/Scripts/ReacherAgent.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MLAgents;
+
+public class ReacherAgent : Agent {
+    public override void CollectObservations()
+    {
+        
+    }
+
+    public override void AgentAction(float[] vectorAction, string textAction)
+    {
+        
+    }
+
+    public override void AgentReset()
+    {
+        
+    }
+}
