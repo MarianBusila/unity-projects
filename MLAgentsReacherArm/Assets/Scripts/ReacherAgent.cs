14870ef6ec8d80199bdf7fe066e674d852de9e1f
Marian Busila <marian.busila@gmail.com>
2018-12-15 20:05:39 -0500

    MLAgentsReacherArm - collect observations for agent

--- MLAgentsReacherArm/Assets/Scripts/ReacherAgent.cs
+++ MLAgentsReacherArm/Assets/Scripts/ReacherAgent.cs
@@ -4,9 +4,42 @@ using UnityEngine;
 using MLAgents;
 
 public class ReacherAgent : Agent {
+    public GameObject upperArm;
+    public GameObject lowerArm;
+    public GameObject hand;
+    public GameObject goal;
+
+    private ReacherAcademy reacherAcademy;
+
+    private Rigidbody upperArmRB;
+    private Rigidbody lowerArmRB;
+
+    private float goalSpeed;
+
+    public override void InitializeAgent()
+    {
+        upperArmRB = upperArm.GetComponent<Rigidbody>();
+        lowerArmRB = lowerArm.GetComponent<Rigidbody>();
+
+        reacherAcademy = GameObject.Find("Academy").GetComponent<ReacherAcademy>();
+    }
+
     public override void CollectObservations()
     {
-        
+        AddVectorObs(upperArm.transform.localPosition);
+        AddVectorObs(upperArm.transform.rotation);
+        AddVectorObs(upperArmRB.angularVelocity);
+        AddVectorObs(upperArmRB.velocity);
+
+        AddVectorObs(lowerArm.transform.localPosition);
+        AddVectorObs(lowerArm.transform.rotation);
+        AddVectorObs(lowerArmRB.angularVelocity);
+        AddVectorObs(lowerArmRB.velocity);
+
+        AddVectorObs(goal.transform.localPosition);
+        AddVectorObs(hand.transform.localPosition);
+
+        AddVectorObs(goalSpeed);
     }
 
     public override void AgentAction(float[] vectorAction, string textAction)
