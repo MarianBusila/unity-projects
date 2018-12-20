6da9900096154c6196cf174248ed30a2e47378b3
Marian Busila <marian.busila@gmail.com>
2018-12-20 13:04:59 -0500

    MLAgentsCrawler - add CrawlerAgent basic script

---
+++ MLAgentsCrawler/Assets/Scripts/CrawlerAgent.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using MLAgents;
+
+public class CrawlerAgent : Agent {
+    [Header("Target to walk towards")]
+    [Space(10)]
+    public Transform target;
+    public Transform ground;
+
+    [Header("Body parts")]
+    [Space(10)]
+    public Transform body;
+    public Transform leg0Upper;
+    public Transform leg0Lower;
+    public Transform leg1Upper;
+    public Transform leg1Lower;
+    public Transform leg2Upper;
+    public Transform leg2Lower;
+    public Transform leg3Upper;
+    public Transform leg3Lower;
+
+    public override void InitializeAgent()
+    {
+        
+    }
+
+    public override void CollectObservations()
+    {
+        
+    }
+
+    public override void AgentAction(float[] vectorAction, string textAction)
+    {
+        
+    }
+
+    public override void AgentReset()
+    {
+        
+    }
+}
