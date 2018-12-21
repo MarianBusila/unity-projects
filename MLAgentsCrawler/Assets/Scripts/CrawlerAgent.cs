9c20eadc2b74d75c87b091e83563a5e84024d04b
Marian Busila <marian.busila@gmail.com>
2018-12-20 21:54:39 -0500

    MLAgentsCrawler - collect observations and take actions for body parts

--- MLAgentsCrawler/Assets/Scripts/CrawlerAgent.cs
+++ MLAgentsCrawler/Assets/Scripts/CrawlerAgent.cs
@@ -42,14 +42,67 @@ public class CrawlerAgent : Agent {
         jdController.SetupBodyPart(leg3Lower);
     }
 
+    /// <summary>
+    /// Add relevant information on each body part to observations.
+    /// </summary>
+    public void CollectObservationBodyPart(BodyPart bp)
+    {
+        var rb = bp.rb;        
+        AddVectorObs(rb.velocity);
+        AddVectorObs(rb.angularVelocity);
+
+        if (bp.rb.transform != body)
+        {
+            Vector3 localPosRelToBody = body.InverseTransformPoint(rb.position);
+            AddVectorObs(localPosRelToBody);
+            AddVectorObs(bp.currentXNormalizedRot); // Current x rot
+            AddVectorObs(bp.currentYNormalizedRot); // Current y rot
+            AddVectorObs(bp.currentZNormalizedRot); // Current z rot
+            AddVectorObs(bp.currentStrength / jdController.maxJointForceLimit);
+        }
+    }
+
     public override void CollectObservations()
     {
-        
+        jdController.GetCurrentJointForces();
+        // Normalize dir vector to help generalize
+        AddVectorObs(dirToTarget.normalized);
+
+        // Forward & up to help with orientation
+        AddVectorObs(body.transform.position.y);
+        AddVectorObs(body.forward);
+        AddVectorObs(body.up);
+        foreach (var bodyPart in jdController.bodyPartsDict.Values)
+        {
+            CollectObservationBodyPart(bodyPart);
+        }
     }
 
     public override void AgentAction(float[] vectorAction, string textAction)
     {
-        
+        // The dictionary with all the body parts in it are in the jdController
+        var bpDict = jdController.bodyPartsDict;
+
+        int i = -1;
+        // Pick a new target joint rotation
+        bpDict[leg0Upper].SetJointTargetRotation(vectorAction[++i], vectorAction[++i], 0);
+        bpDict[leg1Upper].SetJointTargetRotation(vectorAction[++i], vectorAction[++i], 0);
+        bpDict[leg2Upper].SetJointTargetRotation(vectorAction[++i], vectorAction[++i], 0);
+        bpDict[leg3Upper].SetJointTargetRotation(vectorAction[++i], vectorAction[++i], 0);
+        bpDict[leg0Lower].SetJointTargetRotation(vectorAction[++i], 0, 0);
+        bpDict[leg1Lower].SetJointTargetRotation(vectorAction[++i], 0, 0);
+        bpDict[leg2Lower].SetJointTargetRotation(vectorAction[++i], 0, 0);
+        bpDict[leg3Lower].SetJointTargetRotation(vectorAction[++i], 0, 0);
+
+        // Update joint strength
+        bpDict[leg0Upper].SetJointStrength(vectorAction[++i]);
+        bpDict[leg1Upper].SetJointStrength(vectorAction[++i]);
+        bpDict[leg2Upper].SetJointStrength(vectorAction[++i]);
+        bpDict[leg3Upper].SetJointStrength(vectorAction[++i]);
+        bpDict[leg0Lower].SetJointStrength(vectorAction[++i]);
+        bpDict[leg1Lower].SetJointStrength(vectorAction[++i]);
+        bpDict[leg2Lower].SetJointStrength(vectorAction[++i]);
+        bpDict[leg3Lower].SetJointStrength(vectorAction[++i]);
     }
 
     public override void AgentReset()
